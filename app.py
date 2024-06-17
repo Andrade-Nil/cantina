5de215ccee15ce764c716d2321901fff69239e5e
Anderson <anderson@agenciaw3.digital>
2024-06-17 17:22:34 -0400

    change debug

--- app.py
+++ app.py
@@ -449,8 +449,7 @@ def excluir_aluno(aluno_id):
     return jsonify({'success': False, 'error': str(e)}), 500
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
+
+
